aebba683d9da2d5753f80e7e18dae76c606d0964
Arkanic <silverfishasaurus@gmail.com>
2020-04-11 22:44:32 +0000

    pey

---
+++ src/client/networking.js
@@ -0,0 +1,36 @@
+import io from "socket.io-client";
+import {throttle} from "throttle-debounce";
+import {processGameUpdate} from "./state";
+
+const constants = require("../shared/constants");
+
+const socketProtocol = (window.location.protocol.includes("https")) ? "wss" : "ws";
+const socket = io(`${socketProtocol}://${window.location.host}`, {reconnection: false});
+const connectedPromise = new Promise(resolve => {
+    socket.on("connect", () => {
+        console.log("Connected");
+        resolve();
+    });
+});
+
+export const connect = onGameOver => (
+    connectedPromise.then(() => {
+        socket.on(constants.MSG_TYPES.GAME_UPDATE, processGameUpdate);
+        socket.on(constants.MSG_TYPES.GAME_OVER, onGameOver);
+        socket.on("disconnect", () => {
+            console.log("Disconnected");
+            document.getElementById("disconnect-modal").classList.remove("hidden");
+            document.getElementById("reconnect-button").onclick = () => {
+                window.location.reload();
+            };
+        });
+    })
+);
+
+export const play = username => {
+    socket.emit(constants.MSG_TYPES.JOIN_GAME, username);
+};
+
+export const updateDirection = throttle(20, dir => {
+    socket.emit(constants.MSG_TYPES.INPUT, dir);
+});
