6a25d705a27464c5c5881000df9ad67831fd88f2
Arkanic <silverfishasaurus@gmail.com>
2020-09-27 18:47:23 +0000

    port change again

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ if(process.env.NODE_ENV == "development") {
     app.use(express.static("dist"));
 }
 
-const port = process.env.PORT || 8245;
+const port = process.env.PORT || 80;
 const server = app.listen(port);
 console.log("Server Started.")
 
